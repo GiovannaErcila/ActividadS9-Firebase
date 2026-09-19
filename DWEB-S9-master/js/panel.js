d4b0f1ad57acbf1aa05e2441b150f084d3fcb035
Giovanna Ercila <165567694+GiovannaErcila@users.noreply.github.com>
2026-09-19 16:14:01 -0300

    Actividad 9 completa - Firebase Authentication (login, registro, panel protegido, logout)

--- DWEB-S9-master/js/panel.js
+++ DWEB-S9-master/js/panel.js
@@ -7,8 +7,15 @@ const bienvenida = document.querySelector("#bienvenida");
 // Si no hay usuario, redirigí a index.html.
 // Si existe, mostrale: "Bienvenido/a, NOMBRE" en #bienvenida.
 onAuthStateChanged(auth, (usuario) => {
+  if (!usuario) {
+    window.location.href = "index.html";
+  } else {
+    bienvenida.textContent = `Bienvenido/a, ${usuario.displayName}`;
+  }
 });
 
 // Paso 3: Cerrá la sesión con signOut(auth) y redirigí al inicio.
 document.querySelector("#btnCerrarSesion").addEventListener("click", async () => {
+  await signOut(auth);
+  window.location.href = "index.html";
 });
